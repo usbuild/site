b0795e67e8a21d3724e306d7f3741b100d6bd74c
usbuild <njuzhangqichao@gmail.com>
2018-11-03 18:15:36 +0800

    add post

--- themes/lecoding/static/js/main.js
+++ themes/lecoding/static/js/main.js
@@ -1,8 +1,10 @@
 window.addEventListener("load", function(){
     Array.from(document.querySelectorAll("a")).filter(e => e.href.indexOf("gist.github.com") > -1).forEach(a => {
         let icon = document.createElement("i");
+        a.innerText = "GIST";
         icon.classList.add("fab");
         icon.classList.add("fa-github");
         a.prepend(icon);
+        a.classList.add("gist");
     });
 });
